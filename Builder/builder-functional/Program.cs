5ee9147049ec8793d9299eed374eb2201ab41410
MohammadAsgharian <m.asgharian@gmail.com>
2022-04-08 20:56:58 +0430

    master

--- Builder/builder-functional/Program.cs
+++ Builder/builder-functional/Program.cs
@@ -11,18 +11,18 @@ namespace builder_functional
         where TSubject : new()
         where TSelf: FunctionalBuilder<TSubject, TSelf>
         {
-            private readonly List<Func<Person, Person>> actions
-                =new List<Func<Person, Person>>();
+            private readonly List<Func<TSubject, TSubject>> actions
+                =new List<Func<TSubject, TSubject>>();
             
            
             
-            public TSelf Do(Action<Person> action)
+            public TSelf Do(Action<TSubject> action)
                 => AddAction(action);
             
-            public Person Build()
-                => actions.Aggregate(new Person(), (p,f) => f(p));
+            public TSubject Build()
+                => actions.Aggregate(new TSubject(), (p,f) => f(p));
             
-            private TSelf AddAction(Action<Person> action)
+            private TSelf AddAction(Action<TSubject> action)
             {
                 actions.Add(p => {action(p);
                     return p;    
