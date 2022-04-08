a1d4b08ce6adeb4c9deebf5b07d9a2a3cb8611b5
MohammadAsgharian <m.asgharian@gmail.com>
2022-04-08 18:44:26 +0430

    master

--- Builder/builder-functional/Program.cs
+++ Builder/builder-functional/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace builder_functional
 {
+    public class Person{
+        public string Name, Position;
+    }
+    public abstract class FunctionalBuilder<TSubject, TSelf>
+        where TSubject : new()
+        where TSelf: FunctionalBuilder<TSubject, TSelf>
+        {
+            private readonly List<Func<Person, Person>> actions
+                =new List<Func<Person, Person>>();
+            
+           
+            
+            public TSelf Do(Action<Person> action)
+                => AddAction(action);
+            
+            public Person Build()
+                => actions.Aggregate(new Person(), (p,f) => f(p));
+            
+            private TSelf AddAction(Action<Person> action)
+            {
+                actions.Add(p => {action(p);
+                    return p;    
+                });
+                return (TSelf) this;
+            }
+
+        }
+        public sealed class PersonBuilder
+            : FunctionalBuilder<Person, PersonBuilder>
+            {
+                public PersonBuilder Called(string name)
+                    => Do(p => p.Name = name);
+            }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = new PersonBuilder()
+                .Called("name")
+                .Build();
+           
         }
     }
 }
